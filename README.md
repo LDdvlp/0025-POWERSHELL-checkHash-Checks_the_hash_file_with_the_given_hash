94dd1077d14cf526de77bbac486c2faca8dee8f4
LDdvlp <loicdrouet@gmail.com>
2023-10-20 16:45:33 +0200

    v.1.0.0

---
+++ README.md
@@ -0,0 +1,1 @@
+# 0025-POWERSHELL-checkHash-Checks_the_hash_file_with_the_given_hash
---
+++ checkHash.ps1
@@ -0,0 +1,26 @@
+﻿Cls
+
+$FileName = Read-Host -Prompt 'File name'
+$FileLocation = "C:\Users\loicd\Downloads\$FileName"
+Write-Host ""
+
+Write-Host "Choose one of the following file hash"
+Write-Host "SHA1"
+Write-Host "SHA256"
+Write-Host "SHA384"
+Write-Host "SHA512"
+Write-Host "MD5"
+
+$FileHash = Read-Host -Prompt 'Choosen hash'
+
+Get-FileHash $FileLocation -Algorithm $FileHash | Format-List
+
+
+$ExtractedHash = Read-Host -Prompt 'Extracted hash'
+Write-Host ""
+$GivenHash = Read-Host -Prompt 'Given hash'
+Write-Host ""
+$ExtractedHash -eq $GivenHash
+
+
+
